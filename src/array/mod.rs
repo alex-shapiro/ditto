210348b8d50384eaa67aa93ef985886306aa20c4
Alex Shapiro <alexander.max.shapiro@gmail.com>
2016-10-05 17:03:50 -0700

    array tests

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -93,3 +93,98 @@ impl Array {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::element::Element;
+    use Value;
+    use op::LocalOp;
+    use op::local::InsertItem;
+    use op::local::DeleteItem;
+    use std::any::Any;
+
+    #[test]
+    fn test_new() {
+        let array = Array::new();
+        assert!(array.len() == 0);
+        assert!(array.0[0] == Element::start_marker());
+        assert!(array.0[1] == Element::end_marker());
+        assert!(array.len() == 0);
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut array = Array::new();
+        let _  = array.insert(0, Value::Str("a".to_string()), 1, 1);
+        let _  = array.insert(1, Value::Str("b".to_string()), 1, 2);
+        let op = array.insert(1, Value::Str("c".to_string()), 1, 3).unwrap();
+
+        assert!(array.len() == 3);
+        assert!(array.0[1].value == Value::Str("a".to_string()));
+        assert!(array.0[2].value == Value::Str("c".to_string()));
+        assert!(array.0[3].value == Value::Str("b".to_string()));
+
+        assert!(op.inserts.len() == 1);
+        assert!(op.inserts[0].value == Value::Str("c".to_string()));
+        assert!(op.deletes.len() == 0);
+    }
+
+    #[test]
+    fn test_insert_invalid_index() {
+        let mut array = Array::new();
+        assert!(array.insert(1, Value::Bool(true), 1, 1).is_none());
+    }
+
+    #[test]
+    fn test_delete() {
+        let mut array = Array::new();
+        let op1 = array.insert(0, Value::Num(1.0), 1, 1).unwrap();
+        let _   = array.insert(1, Value::Num(2.0), 1, 1);
+        let op2 = array.delete(0).unwrap();
+
+        assert!(array.0[1].value == Value::Num(2.0));
+        assert!(op2.deletes[0] == op1.inserts[0].uid);
+    }
+
+    #[test]
+    fn test_delete_invalid_index() {
+        let mut array = Array::new();
+        array.insert(0, Value::Num(1.0), 1, 1);
+        assert!(array.delete(1).is_none());
+    }
+
+    #[test]
+    fn test_execute_remote() {
+        let mut array1 = Array::new();
+        let mut array2 = Array::new();
+
+        let op1 = array1.insert(0, Value::Num(1.0), 1, 1).unwrap();
+        let op2 = array1.insert(1, Value::Num(2.0), 1, 2).unwrap();
+        let op3 = array1.delete(0).unwrap();
+        let lops1 = array2.execute_remote(op1);
+        let lops2 = array2.execute_remote(op2);
+        let lops3 = array2.execute_remote(op3);
+
+        assert!(array1 == array2);
+        assert!(lops1.len() == 1);
+        assert!(lops2.len() == 1);
+        assert!(lops3.len() == 1);
+
+        let lop1 = to::<InsertItem>(&lops1);
+        assert!(lop1.index == 0);
+        assert!(lop1.value == Value::Num(1.0));
+
+        let lop2 = to::<InsertItem>(&lops2);
+        assert!(lop2.index == 1);
+        assert!(lop2.value == Value::Num(2.0));
+
+        let lop3 = to::<DeleteItem>(&lops3);
+        assert!(lop3.index == 0);
+    }
+
+    fn to<'a, T: Any>(ops: &'a [Box<LocalOp>]) -> &'a T {
+        let op = &ops[0];
+        op.as_any().downcast_ref::<T>().unwrap()
+    }
+}
