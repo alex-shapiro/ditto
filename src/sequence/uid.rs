b31e4c3ba3a20b9593843298d98bc2a5e07cb8d0
Alex Shapiro <alexander.max.shapiro@gmail.com>
2016-10-13 12:58:21 -0700

    new and improved sequence UID

--- src/sequence/uid.rs
+++ src/sequence/uid.rs
@@ -1,11 +1,29 @@
+//! A sequence UID is an unique identifier that maintains
+//! a total order of sequential elements in a distributed
+//! environment.
+//!
+//! ## Implementation Notes
+//!
+//! New UIDs are generated with a variant of the LSEQ
+//! algorithm. It diverges from the original paper by
+//! choosing allocation strategy deterministically rather
+//! than according to a random coin flip. Randomized
+//! allocation strategies cannot converge in a distributed
+//! environment because replicas cannot reliably coordinate
+//! strategies, and so UID bit size increases quickly with
+//! the number of sequence elements. However, a
+//! deterministic allocation strategy known by all replicas
+//! ahead of time is inherently coordinated, and UID bit
+//! size increases more slowly with the number of elements.
+
 use std::cmp;
 use std::cmp::Ordering;
 use rand;
 use rand::distributions::{IndependentSample, Range};
 use num::bigint::{BigUint, ToBigUint};
 use num::cast::ToPrimitive;
-use num::Zero;
-
+use std::fmt;
+use std::fmt::Debug;
 use Replica;
 
 const BASE_LEVEL: usize = 3;
@@ -14,93 +32,97 @@ const BOUNDARY:   usize = 10;
 
 #[derive(Clone,PartialEq,Eq,Ord)]
 pub struct UID {
-    positions: BigUint,
+    position: BigUint,
     site: u32,
     counter: u32,
 }
 
 impl UID {
+    fn new(position: BigUint, site: u32, counter: u32) -> Self {
+        UID{position: position, site: site, counter: counter}
+    }
+
     pub fn min() -> Self {
-        UID{positions: BigUint::zero(), site: 0, counter: 0}
+        UID::new(big(1 << BASE_LEVEL), 0, 0)
     }
 
     pub fn max() -> Self {
-        let positions = big(1 << BASE_LEVEL);
-        UID{positions: positions, site: 0, counter: 0}
+        let position = big((1 << BASE_LEVEL+1) - 1);
+        UID::new(position, u32::max_value(), u32::max_value())
     }
 
-    /// Creates a new UID that falls between uid1 and uid2
     pub fn between(uid1: &UID, uid2: &UID, replica: &Replica) -> Self {
-        let mut positions1 = uid1.positions.clone();
-        let mut positions2 = uid2.positions.clone();
-        let mut positions = BigUint::zero();
+        let ref position1       = uid1.position;
+        let ref position2       = uid2.position;
+        let mut position        = big(1);
+        let mut significant_bits = 1;
 
         for level in BASE_LEVEL..(MAX_LEVEL+1) {
-            let pos1 = get_pos(&positions1, level).unwrap_or(0);
-            let pos2 = get_pos(&positions2, level).unwrap_or(1 << level);
+            significant_bits += level;
+            let pos1 = UID::get_pos(position1, level, significant_bits).unwrap_or(0);
+            let pos2 = UID::get_pos(position2, level, significant_bits).unwrap_or((1 << level) - 1);
 
             if pos1 + 1 < pos2 {
-                let pos = generate_pos(pos1, pos2, level);
-                positions = (positions << level) + big(pos);
-                return UID{
-                    positions: positions,
-                    site: replica.site,
-                    counter: replica.counter,
-                }
+                let pos = UID::generate_pos(pos1, pos2, level);
+                position = (position << level) + big(pos);
+                return UID::new(position, replica.site, replica.counter);
             } else {
-                positions = (positions << level) + big(pos1);
-                positions1 = positions1 >> level;
-                positions2 = positions2 >> level;
+                position = (position << level) + big(pos1);
             }
         }
-        panic!(format!("Can't have more than {} levels in a UID!", MAX_LEVEL));
+        panic!(format!("UID cannot have more than ({}) levels", MAX_LEVEL));
     }
-}
 
-/// Gets the next position in a positions BigUint, if
-/// the next position exists. The lowest position is
-/// the `level` lowest bits in the BigUint.
-fn get_pos(positions: &BigUint, level: usize) -> Option<usize> {
-    let bits = positions.bits();
-    if bits >= level {
-        let mask = (BigUint::zero() << (level+1)) - big(1);
-        let pos = (positions & mask).to_usize().unwrap();
-        Some(pos)
-    } else {
-        None
-    }
-}
-
-/// Generates an position that falls between pos1 and pos2.
-/// Uses either boundary+ or boundary- strategy.
-///
-/// boundary+ is used on odd levels and returns an integer
-/// from interval [pos1+1, min(pos1+BOUNDARY, pos2-1)]
-///
-/// boundary- is used on even levels and returns an integer
-/// from interval [max(pos1+1, pos2-BOUNDARY), pos2-1]
-///
-fn generate_pos(pos1: usize, pos2: usize, level: usize) -> usize {
-    let range =
-        if use_plus_strategy(level) {
-            let lo_bound = pos1+1;
-            let hi_bound = cmp::min(pos1+BOUNDARY, pos2);
-            Range::new(lo_bound, hi_bound)
-        } else if pos2 <= BOUNDARY {
-            let lo_bound = pos1+1;
-            let hi_bound = pos2;
-            Range::new(lo_bound, hi_bound)
+    /// Gets the value for a particular level in a position
+    /// if the position has a value for that level. A position's
+    /// most significant bit is not part of any level (it
+    /// is a placeholder to prevent the highest level from being
+    /// truncated). The next 3 most significant bits form level 3,
+    /// the next 4 most significant bits form level 4; and so on.
+    fn get_pos(position: &BigUint, level: usize, significant_bits: usize) -> Option<usize> {
+        let bits = position.bits();
+        if bits >= significant_bits {
+            let insignificant_bits = position.bits() - significant_bits;
+            let level_mask = big((1 << level) - 1);
+            let pos = (position >> insignificant_bits) & level_mask;
+            Some(pos.to_usize().unwrap())
         } else {
-            let lo_bound = cmp::max(pos1+1, pos2-BOUNDARY);
-            let hi_bound = pos2;
-            Range::new(lo_bound, hi_bound)
-        };
-    let mut rng = rand::thread_rng();
-    range.ind_sample(&mut rng)
-}
+            None
+        }
+    }
 
-fn use_plus_strategy(level: usize) -> bool {
-    level % 2 == 1
+    /// Generates a number that falls between pos1 and pos2.
+    /// It requires pos1 + 1 < pos2. Uses boundary+ strategy
+    /// on odd levels and boundary+ strategy on even levels.
+    ///
+    /// boundary+ returns an integer in the interval
+    /// [pos1+1, min(pos1+BOUNDARY, pos2-1)]
+    ///
+    /// boundary- returns an integer in the interval
+    /// [max(pos1+1, pos2-BOUNDARY), pos2-1]
+    ///
+    fn generate_pos(pos1: usize, pos2: usize, level: usize) -> usize {
+        let range =
+            if UID::use_boundary_plus_strategy(level) {
+                let lo_bound = pos1+1;
+                let hi_bound = cmp::min(pos1+BOUNDARY, pos2);
+                Range::new(lo_bound, hi_bound)
+            } else if pos2 <= BOUNDARY {
+                let lo_bound = pos1+1;
+                let hi_bound = pos2;
+                Range::new(lo_bound, hi_bound)
+            } else {
+                let lo_bound = cmp::max(pos1+1, pos2-BOUNDARY);
+                let hi_bound = pos2;
+                Range::new(lo_bound, hi_bound)
+            };
+        let mut rng = rand::thread_rng();
+        range.ind_sample(&mut rng)
+    }
+
+    fn use_boundary_plus_strategy(level: usize) -> bool {
+        level % 2 == 1
+    }
 }
 
 fn big(num: usize) -> BigUint {
@@ -109,22 +131,23 @@ fn big(num: usize) -> BigUint {
 
 impl PartialOrd for UID {
     fn partial_cmp(&self, other: &UID) -> Option<Ordering> {
-        let mut self_positions = self.positions.clone();
-        let mut other_positions = other.positions.clone();
+        let mut self_position = self.position.clone();
+        let mut other_position = other.position.clone();
 
-        let self_bits = self_positions.bits();
-        let other_bits = other_positions.bits();
+        let self_bits = self_position.bits();
+        let other_bits = other_position.bits();
 
         // truncate
         if self_bits > other_bits {
-            self_positions = self_positions >> (self_bits - other_bits);
+            self_position = self_position >> (self_bits - other_bits);
         } else {
-            other_positions = other_positions >> (other_bits - self_bits);
+            other_position = other_position >> (other_bits - self_bits);
         }
 
-        if self_positions < other_positions {
+        // compare
+        if self_position < other_position {
             Some(Ordering::Less)
-        } else if self_positions > other_positions {
+        } else if self_position > other_position {
             Some(Ordering::Greater)
         } else if self_bits < other_bits {
             Some(Ordering::Less)
@@ -141,5 +164,138 @@ impl PartialOrd for UID {
         } else {
             Some(Ordering::Equal)
         }
+    }
+}
+
+impl Debug for UID {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let uid = self.position.to_str_radix(2);
+        write!(f, "<{}, {}, {}>", uid, self.site, self.counter)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num::bigint::{BigUint, ToBigUint};
+    use Replica;
+
+    const REPLICA: Replica = Replica{site: 3, counter: 2};
+
+    #[test]
+    fn test_min() {
+        let uid = UID::min();
+        assert!(uid.position == big(0b1000));
+        assert!(uid.site == 0);
+        assert!(uid.counter == 0);
+    }
+
+    #[test]
+    fn test_max() {
+        let uid = UID::max();
+        assert!(uid.position == big(0b1111));
+        assert!(uid.site == 4294967295);
+        assert!(uid.counter == 4294967295);
+    }
+
+    #[test]
+    fn test_ord() {
+        let uid1 = UID::min();
+        let uid2 = UID{position: big(0b1010101), site: 1, counter: 5};
+        let uid3 = UID{position: big(0b1010101), site: 1, counter: 5};
+        let uid4 = UID{position: big(0b1101111), site: 4, counter: 4};
+        let uid5 = UID{position: big(0b11011111), site: 4, counter: 4};
+        let uid6 = UID::max();
+
+        assert!(uid1 < uid2);
+        assert!(uid2 == uid3);
+        assert!(uid3 < uid4);
+        assert!(uid4 < uid5);
+        assert!(uid5 < uid6);
+    }
+
+    #[test]
+    fn test_between_trivial() {
+        let uid1 = UID::min();
+        let uid2 = UID::max();
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+
+        assert!(big(0b1_000) < uid.position);
+        assert!(big(0b1_111) > uid.position);
+        assert!(uid.site == 3);
+        assert!(uid.counter == 2);
+    }
+
+    #[test]
+    fn test_between_basic() {
+        let uid1 = UID{position: big(0b1_010), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_100), site: 1, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(uid.position == big(0b1011));
+    }
+
+    #[test]
+    fn test_between_multi_level() {
+        let uid1 = UID{position: big(0b1_100_0001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_100_0011), site: 1, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_100_0010) == uid.position);
+    }
+
+    #[test]
+    fn test_between_squeeze() {
+        let uid1 = UID{position: big(0b1_010_0100_00100), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_011_0101_00110), site: 1, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_010_0100_00101) == uid.position);
+    }
+
+    #[test]
+    fn test_between_boundary_plus() {
+        let uid1 = UID{position: big(0b1_001_0001_00001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_001_0001_11101), site: 1, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_001_0001_00001) < uid.position);
+        assert!(big(0b1_001_0001_01101) > uid.position);
+    }
+
+    #[test]
+    fn test_between_boundary_minus() {
+        let uid1 = UID{position: big(0b1_001_0001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_001_1111), site: 1, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_001_0100) < uid.position);
+        assert!(big(0b1_001_1111) > uid.position);
+    }
+
+    #[test]
+    fn test_between_equals() {
+        let uid1 = UID{position: big(0b1_001_0001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_001_0001), site: 2, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_001_0001_00000) < uid.position);
+        assert!(big(0b1_001_0001_01011) > uid.position);
+    }
+
+    #[test]
+    fn test_between_first_is_shorter() {
+        let uid1 = UID{position: big(0b1_001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_001_0001), site: 2, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_001_0000_00000) < uid.position);
+        assert!(big(0b1_001_0000_01011) > uid.position);
+    }
+
+    #[test]
+    fn test_between_first_is_longer() {
+        let uid1 = UID{position: big(0b1_001_0001), site: 1, counter: 1};
+        let uid2 = UID{position: big(0b1_010), site: 2, counter: 1};
+        let uid  = UID::between(&uid1, &uid2, &REPLICA);
+        assert!(big(0b1_001_0100) < uid.position);
+        assert!(big(0b1_001_1111) > uid.position);
+    }
+
+    fn big(num: usize) -> BigUint {
+        num.to_biguint().unwrap()
     }
 }
