b073d0fa072eaa96b8cb8a041f8bb8749a005934
Alex Shapiro <alexander.max.shapiro@gmail.com>
2016-10-14 14:47:40 -0700

    redo range

--- src/attributed_string/element.rs
+++ src/attributed_string/element.rs
@@ -1,4 +1,3 @@
-use super::attr::{AttrOpen,AttrClose};
 use sequence::uid::UID;
 use Replica;
 
@@ -6,8 +5,6 @@ use Replica;
 pub enum EltValue {
     None,
     Text(String),
-    AttrOpen(AttrOpen),
-    AttrClose(AttrClose),
 }
 
 impl EltValue {
@@ -47,20 +44,17 @@ impl Element {
         Self::new(EltValue::Text(value), uid)
     }
 
-    pub fn new_attr_open(key: String, value: String, uid: UID) -> Self {
-        let attr_open = AttrOpen::new(key, value);
-        Self::new(EltValue::AttrOpen(attr_open), uid)
-    }
-
-    pub fn new_attr_close(key: String, uid: UID) -> Self {
-        let attr_close = AttrClose::new(key);
-        Self::new(EltValue::AttrClose(attr_close), uid)
-    }
-
     pub fn is_marker(&self) -> bool {
         match &self.value {
             &EltValue::None => true,
             _ => false
+        }
+    }
+
+    pub fn is_end_marker(&self) -> bool {
+        match &self.value {
+            &EltValue::None => (self.uid == UID::max()),
+            _ => false,
         }
     }
 
@@ -71,37 +65,9 @@ impl Element {
         }
     }
 
-    pub fn is_attr_open(&self) -> bool {
-        match &self.value {
-            &EltValue::AttrOpen(_) => true,
-            _ => false
-        }
-    }
-
-    pub fn is_attr_close(&self) -> bool {
-        match &self.value {
-            &EltValue::AttrClose(_) => true,
-            _ => false
-        }
-    }
-
     pub fn text(&self) -> Option<&str> {
         match &self.value {
             &EltValue::Text(ref str) => Some(str),
-            _ => None,
-        }
-    }
-
-    pub fn attr_open(&self) -> Option<&AttrOpen> {
-        match &self.value {
-            &EltValue::AttrOpen(ref attr_open) => Some(attr_open),
-            _ => None,
-        }
-    }
-
-    pub fn attr_close(&self) -> Option<&AttrClose> {
-        match &self.value {
-            &EltValue::AttrClose(ref attr_close) => Some(attr_close),
             _ => None,
         }
     }
--- src/attributed_string/range.rs
+++ src/attributed_string/range.rs
@@ -1,122 +1,46 @@
+//! A half-open interval over a sequence of AttributedString
+//! elements, from a lower bound up to, but not including, an
+//! upper bound. A complication is that a bound may be split,
+//! indicating that the range includes *part* but not all of
+//! an element.
+
 use super::element::Element;
 
-#[derive(Clone,PartialEq,PartialOrd,Debug)]
-pub enum Index {
-    Whole{index: usize},
-    Part{index: usize, offset: usize}
+#[derive(Debug,PartialEq)]
+pub struct Bound {
+    pub index: usize,
+    pub offset: usize,
 }
 
-impl Index {
-    fn new(index: usize, offset: usize) -> Self {
-        match offset {
-            0 => Index::Whole{index: index},
-            _ => Index::Part{index: index, offset: offset},
+impl Bound {
+    pub fn get(elements: &[Element], char_index: usize) -> Self {
+        let mut current_char_index = 0;
+        for (index, elt) in elements.iter().enumerate() {
+            if current_char_index + elt.len() > char_index {
+                let offset = char_index - current_char_index;
+                return Bound{index:index, offset: offset};
+            } else if elt.is_end_marker() {
+                return Bound{index: index, offset: 0};
+            }
+            current_char_index += elt.len();
         }
+        Bound{index: elements.len() - 1, offset: 0}
     }
 }
 
 #[derive(Debug)]
 pub struct Range {
-    pub start: Index,
-    pub stop: Index,
+    pub lower: Bound,
+    pub upper: Bound,
 }
 
 impl Range {
-    /// Create a new Range that excludes all attributes bounding the
-    /// in-range text. Used in operations that don't affect
-    /// bounding attributes (ie text insert/replace).
-    pub fn excluding_boundary_attrs(elements: &[Element], start: usize, len: usize) -> Self {
-        let stop = start + len;
-        let mut in_range = false;
-        let mut char_index:   usize = 0;
-        let mut elt_index:    usize = 0;
-
-        let mut start_index:  usize = 0;
-        let mut start_offset: usize = 0;
-        let mut stop_index:   usize = 0;
-        let mut stop_offset:  usize = 0;
-
-        for elt in elements {
-            let elt_len = elt.len();
-            if !in_range {
-                if char_index + elt_len > start {
-                    start_index = elt_index;
-                    start_offset = start - char_index;
-                    in_range = true;
-                } else if elt.text().is_some() {
-                    start_index = elt_index;
-                }
-            } else {
-                if char_index + elt_len > stop {
-                    stop_index = elt_index;
-                    stop_offset = stop - char_index;
-                    break;
-                } else if char_index + elt_len == stop {
-                    stop_index = elt_index;
-                    break;
-                } else if elt.text().is_some() {
-                    stop_index = elt_index;
-                }
-            }
-            char_index += elt_len;
-            elt_index += 1;
-        }
-        Range::new(start_index, start_offset, stop_index, stop_offset)
-    }
-
-    /// Create a new Range that includes all attributes bounding the
-    /// in-range text. Used in operations that affect bounding
-    /// attributes (ie text delete, attribute put/delete)
-    pub fn including_boundary_attrs(elements: &[Element], start: usize, len: usize) -> Self {
-        let stop = start + len;
-        let mut in_range = false;
-        let mut char_index:   usize = 0;
-        let mut elt_index:    usize = 0;
-
-        let mut start_index:  usize = 0;
-        let mut start_offset: usize = 0;
-        let mut stop_index:   usize = 0;
-        let mut stop_offset:  usize = 0;
-
-        for elt in elements {
-            let elt_len = elt.len();
-
-            if !in_range {
-                if char_index + elt_len > start {
-                    start_index = elt_index;
-                    start_offset = start - char_index;
-                    in_range = true;
-                } else if char_index == start && elt.is_attr_open() {
-                    start_index = elt_index;
-                    in_range = true;
-                } else if elt.is_attr_open() || elt.is_text() {
-                    start_index = elt_index;
-                }
-            } else {
-                if char_index == stop && !elt.is_attr_close() {
-                    stop_index = elt_index - 1;
-                    break;
-                } else if char_index + elt_len > stop {
-                    stop_index = elt_index;
-                    stop_offset = stop - char_index;
-                    break;
-                } else if elt.is_text() || elt.is_attr_open() {
-                    stop_index = elt_index;
-                }
-            }
-            char_index += elt_len;
-            elt_index += 1;
-        }
-        Range::new(start_index, start_offset, stop_index, stop_offset)
-    }
-
-    fn new(start_index: usize, start_offset: usize, stop_index: usize, stop_offset: usize) -> Self {
-        let start = Index::new(start_index, start_offset);
-        let stop = Index::new(stop_index, stop_offset);
-        match start <= stop {
-            true  => Range{start: start, stop: stop},
-            false => Range{start: start.clone(), stop: start},
-        }
+    pub fn new(elements: &[Element], start: usize, len: usize) -> Self {
+        let lower = Bound::get(elements, start);
+        let elements_rest = &elements[lower.index..elements.len()];
+        let mut upper = Bound::get(elements_rest, len + lower.offset);
+        upper.index += lower.index;
+        Range{lower: lower, upper: upper}
     }
 }
 
@@ -124,87 +48,73 @@ impl Range {
 mod tests {
     use super::*;
     use super::super::element::*;
-    use super::super::attr::*;
     use sequence::uid::UID;
     use Replica;
 
     #[test]
-    fn test_new() {
-        let range = Range::new(43, 0, 53, 3);
-        assert!(range.start == Index::Whole{index: 43});
-        assert!(range.stop == Index::Part{index: 53, offset: 3});
+    fn test_new_all_elements() {
+        let elements = test_elements();
+        let range = Range::new(&elements, 0, 44);
+        assert!(range.lower == Bound{index: 1, offset: 0});
+        assert!(range.upper == Bound{index: 10, offset: 0});
     }
 
     #[test]
-    fn test_excluding_boundary_attrs_0() {
+    fn test_new_no_split_bounds() {
         let elements = test_elements();
-        let range = Range::excluding_boundary_attrs(&elements, 0, 17);
-        assert!(range.start == Index::Whole{index: 1});
-        assert!(range.stop == Index::Whole{index: 6});
+        let range = Range::new(&elements, 10, 16);
+        assert!(range.lower == Bound{index: 3, offset: 0});
+        assert!(range.upper == Bound{index: 6, offset: 0});
     }
 
     #[test]
-    fn test_excluding_boundary_attrs_1() {
+    fn test_new_split_lower_bound() {
         let elements = test_elements();
-        let range = Range::excluding_boundary_attrs(&elements, 6, 11);
-        assert!(range.start == Index::Whole{index: 4});
-        assert!(range.stop == Index::Whole{index: 6});
+        let range = Range::new(&elements, 11, 20);
+        assert!(range.lower == Bound{index: 3, offset: 1});
+        assert!(range.upper == Bound{index: 7, offset: 0});
     }
 
     #[test]
-    fn test_excluding_boundary_attrs_2() {
+    fn test_new_split_upper_bound() {
         let elements = test_elements();
-        let range = Range::excluding_boundary_attrs(&elements, 4, 8);
-        assert!(range.start == Index::Part{index: 1, offset: 4});
-        assert!(range.stop == Index::Part{index: 6, offset: 3});
+        let range = Range::new(&elements, 4, 10);
+        assert!(range.lower == Bound{index: 2, offset: 0});
+        assert!(range.upper == Bound{index: 3, offset: 4});
     }
 
     #[test]
-    fn test_including_boundary_attrs_0() {
+    fn test_new_single_element_split() {
         let elements = test_elements();
-        let range = Range::including_boundary_attrs(&elements, 0, 17);
-        assert!(range.start == Index::Whole{index: 1});
-        assert!(range.stop == Index::Whole{index: 7});
+        let range = Range::new(&elements, 21, 2);
+        assert!(range.lower == Bound{index: 5, offset: 1});
+        assert!(range.upper == Bound{index: 5, offset: 3});
     }
 
     #[test]
-    fn test_including_boundary_attrs_1() {
+    fn test_new_zero_length_range() {
         let elements = test_elements();
-        let range = Range::including_boundary_attrs(&elements, 6, 3);
-        assert!(range.start == Index::Whole{index: 2});
-        assert!(range.stop == Index::Whole{index: 5});
-    }
-
-    #[test]
-    fn test_including_boundary_attrs_2() {
-        let elements = test_elements();
-        let range = Range::including_boundary_attrs(&elements, 4, 12);
-        assert!(range.start == Index::Part{index: 1, offset: 4});
-        assert!(range.stop == Index::Part{index: 6, offset: 7});
+        let range = Range::new(&elements, 21, 0);
+        assert!(range.lower == Bound{index: 5, offset: 1});
+        assert!(range.upper == Bound{index: 5, offset: 1});
     }
 
     fn test_elements() -> Vec<Element> {
         build_elements(vec![
-            text("hello "),
-            attropen("A",""),
-            attropen("B",""),
-            text("and"),
-            attrclose("B"),
-            text(" goodbye"),
-            attrclose("A"),
+            text("The "),
+            text("quick "),
+            text("brown "),
+            text("fox "),
+            text("jumps "),
+            text("over "),
+            text("the "),
+            text("lazy "),
+            text("dog."),
         ])
     }
 
     fn text(value: &str) -> EltValue {
         EltValue::Text(value.to_string())
-    }
-
-    fn attropen(key: &str, value: &str) -> EltValue {
-        EltValue::AttrOpen(AttrOpen::new(key.to_string(), value.to_string()))
-    }
-
-    fn attrclose(key: &str) -> EltValue {
-        EltValue::AttrClose(AttrClose::new(key.to_string()))
     }
 
     fn build_elements(elt_values: Vec<EltValue>) -> Vec<Element> {
