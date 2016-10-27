6eceb816e54166c51b2be67c0a76a6b42f6310b6
Alex Shapiro <alexander.max.shapiro@gmail.com>
2016-10-26 22:08:30 -0700

    Serde support for NestedLocalOp (#13)
    Implement serde Serialize and Deserialize for NestedLocalOp. This allows automatic serialization of NestedLocalOp instances that form part of a larger message.

--- src/op/mod.rs
+++ src/op/mod.rs
@@ -3,6 +3,8 @@ pub mod remote;
 
 pub use self::local::LocalOp;
 pub use self::remote::RemoteOp;
+use raw;
+use serde;
 
 pub struct NestedLocalOp {
     pub pointer: String,
@@ -12,4 +14,11 @@ pub struct NestedLocalOp {
 pub struct NestedRemoteOp {
     pub pointer: String,
     pub op: remote::RemoteOp,
+}
+
+impl serde::Serialize for NestedLocalOp {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: serde::Serializer {
+        serializer.serialize_some(raw::encode_op(self))
+    }
 }
