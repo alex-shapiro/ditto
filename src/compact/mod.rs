f640d7caef57f4027ed42ab0d298290c0eb45295
Alex Shapiro <alexander.max.shapiro@gmail.com>
2016-10-26 21:17:22 -0700

    remove warnings from tests

--- src/compact/mod.rs
+++ src/compact/mod.rs
@@ -63,11 +63,11 @@ mod tests {
     #[test]
     fn test_attributed_string() {
         let mut string = AttributedString::new();
-        string.insert_text(0,  "the ".to_string(), &Replica::new(1, 1));
-        string.insert_text(4,  "quick ".to_string(), &Replica::new(1, 2));
-        string.insert_text(16, "brown ".to_string(), &Replica::new(1, 3));
-        string.insert_text(22, "fox ".to_string(), &Replica::new(1, 4));
-        string.insert_text(26, "jumped".to_string(), &Replica::new(1, 5));
+        let _ = string.insert_text(0,  "the ".to_string(), &Replica::new(1, 1));
+        let _ = string.insert_text(4,  "quick ".to_string(), &Replica::new(1, 2));
+        let _ = string.insert_text(16, "brown ".to_string(), &Replica::new(1, 3));
+        let _ = string.insert_text(22, "fox ".to_string(), &Replica::new(1, 4));
+        let _ = string.insert_text(26, "jumped".to_string(), &Replica::new(1, 5));
 
         let original = Value::AttrStr(string);
         let encoded  = encode_str(&original);
@@ -78,11 +78,11 @@ mod tests {
     #[test]
     fn test_array() {
         let mut array = Array::new();
-        array.insert(0, Value::Null, &Replica::new(1, 1));
-        array.insert(1, Value::Bool(true), &Replica::new(2, 1));
-        array.insert(2, Value::Num(-132.0), &Replica::new(14, 3));
-        array.insert(3, Value::Str("x".to_string()), &Replica::new(48, 84));
-        array.insert(4, Value::Bool(false), &Replica::new(1, 552));
+        let _ = array.insert(0, Value::Null, &Replica::new(1, 1));
+        let _ = array.insert(1, Value::Bool(true), &Replica::new(2, 1));
+        let _ = array.insert(2, Value::Num(-132.0), &Replica::new(14, 3));
+        let _ = array.insert(3, Value::Str("x".to_string()), &Replica::new(48, 84));
+        let _ = array.insert(4, Value::Bool(false), &Replica::new(1, 552));
 
         let original = Value::Arr(array);
         let encoded  = encode_str(&original);
@@ -93,11 +93,11 @@ mod tests {
     #[test]
     fn test_object() {
         let mut object = Object::new();
-        object.put("", Value::Null, &Replica::new(1, 1));
-        object.put("a", Value::Bool(true), &Replica::new(2, 1));
-        object.put("__TYPE__", Value::Num(-132.0), &Replica::new(14, 3));
-        object.put("~0", Value::Str("x".to_string()), &Replica::new(48, 84));
-        object.put("x/y", Value::Bool(false), &Replica::new(1, 552));
+        let _ = object.put("", Value::Null, &Replica::new(1, 1));
+        let _ = object.put("a", Value::Bool(true), &Replica::new(2, 1));
+        let _ = object.put("__TYPE__", Value::Num(-132.0), &Replica::new(14, 3));
+        let _ = object.put("~0", Value::Str("x".to_string()), &Replica::new(48, 84));
+        let _ = object.put("x/y", Value::Bool(false), &Replica::new(1, 552));
 
         let original = Value::Obj(object);
         let encoded  = encode_str(&original);
@@ -108,9 +108,9 @@ mod tests {
     #[test]
     fn test_nested() {
         let mut array   = Array::new();
-        array.insert(0, Value::object(), &Replica::new(34,2));
-        array.insert(1, Value::attrstr(), &Replica::new(392,12));
-        array.insert(2, Value::array(), &Replica::new(4782,4));
+        let _ = array.insert(0, Value::object(), &Replica::new(34,2));
+        let _ = array.insert(1, Value::attrstr(), &Replica::new(392,12));
+        let _ = array.insert(2, Value::array(), &Replica::new(4782,4));
 
         let original = Value::Arr(array);
         let encoded  = encode_str(&original);
