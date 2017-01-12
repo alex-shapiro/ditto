115fa6083b9e3f20c26f62740aa8153b1c5ea7b1
Alex Shapiro <alexander.max.shapiro@gmail.com>
2017-01-12 09:56:41 -0800

    split child node

--- src/attributed_string/btree.rs
+++ src/attributed_string/btree.rs
@@ -1,12 +1,13 @@
 use super::element::Element;
 use error::Error;
+use std::mem;
 
 const B: usize = 6;
 const MIN_LEN: usize = B - 1;
 const CAPACITY: usize = 2 * B - 1;
 
 struct Node {
-    chars: usize,
+    len: usize,
     leaf: bool,
     elements: Vec<Element>,
     children: Vec<Node>,
@@ -18,12 +19,12 @@ impl Node {
     /// Otherwise returns a reference to the node and
     /// the offset of the index inside the node.
     pub fn search(&self, index: usize) -> Result<(&Node, usize), Error> {
-        if index >= self.chars { return Err(Error::OutOfBounds) }
+        if index >= self.len { return Err(Error::OutOfBounds) }
 
         let mut i = index;
         for child in &self.children {
-            if i >= child.chars {
-                i -= child.chars
+            if i >= child.len {
+                i -= child.len
             } else if child.leaf {
                 return Ok((&child, i))
             } else {
@@ -32,5 +33,38 @@ impl Node {
         }
 
         unreachable!()
+    }
+
+    /// Split the node's ith child in half. The original child MUST
+    /// be full (ie, it contains 2B-1 elements and 2B children).
+    /// Each new child has B-1 elements and B children. The original
+    /// child's median element is promoted as the ith element of the
+    /// parent node.
+    fn split_child(&mut self, i: usize) {
+        let (median, new_child) = {
+            let ref mut child = self.children[i];
+            let mut elements = child.elements.split_off(MIN_LEN);
+            let mut children = match child.leaf {
+                true  => vec![],
+                false => child.children.split_off(B),
+            };
+
+            let median = elements.remove(0);
+
+            let mut new_child_len = elements.iter().map(|e| e.len).sum();
+            new_child_len += children.iter().map(|e| e.len).sum();
+            let mut new_child = Node{
+                len: new_child_len,
+                leaf: child.leaf,
+                elements: elements,
+                children: children,
+            };
+
+            child.len -= new_child.len + median.len;
+            (median, new_child)
+        };
+
+        self.elements.insert(i, median);
+        self.children.insert(i + 1, new_child);
     }
 }
