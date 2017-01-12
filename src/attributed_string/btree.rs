f4e1370105286322c63239459b9759558e8a0483
Alex Shapiro <alexander.max.shapiro@gmail.com>
2017-01-12 08:49:04 -0800

    improve btree

--- src/attributed_string/btree.rs
+++ src/attributed_string/btree.rs
@@ -1,42 +1,30 @@
 use super::element::Element;
 use error::Error;
 
-struct Node{
-    len: usize,
-    height: usize,
-    value: NodeVal,
-}
+const B: usize = 6;
+const MIN_LEN: usize = B - 1;
+const CAPACITY: usize = 2 * B - 1;
 
-enum NodeVal {
-    Leaf(Element),
-    Internal(Vec<Node>),
+struct Node {
+    chars: usize,
+    leaf: bool,
+    elements: Vec<Element>,
+    children: Vec<Node>,
 }
-
 
 impl Node {
-    fn is_leaf(&self) -> bool {
-        self.height == 0
-    }
-
-    fn children(&self) -> &[Node] {
-        match self.value {
-            NodeVal::Internal(ref children) => children,
-            NodeVal::Leaf(_) => panic!("can't get children of a leaf!")
-        }
-    }
-
     /// finds the node that contains `index`.
     /// If the index is out of bounds, returns an error.
     /// Otherwise returns a reference to the node and
     /// the offset of the index inside the node.
     pub fn search(&self, index: usize) -> Result<(&Node, usize), Error> {
-        if index >= self.len { return Err(Error::OutOfBounds) }
+        if index >= self.chars { return Err(Error::OutOfBounds) }
 
         let mut i = index;
-        for child in self.children() {
-            if i >= child.len {
-                i -= child.len
-            } else if child.is_leaf() {
+        for child in &self.children {
+            if i >= child.chars {
+                i -= child.chars
+            } else if child.leaf {
                 return Ok((&child, i))
             } else {
                 return child.search(i);
