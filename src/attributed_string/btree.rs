9646f95831535d2fd78f00557cc966c226094eaa
Alex Shapiro <alexander.max.shapiro@gmail.com>
2017-01-12 12:11:05 -0800

    insert a node into a tree

--- src/attributed_string/btree.rs
+++ src/attributed_string/btree.rs
@@ -6,12 +6,44 @@ const B: usize = 6;
 const MIN_LEN: usize = B - 1;
 const CAPACITY: usize = 2 * B - 1;
 
+struct BTree {
+    root: Node,
+}
+
 struct Node {
     len: usize,
     leaf: bool,
     elements: Vec<Element>,
     children: Vec<Node>,
 }
+
+impl BTree {
+    pub fn new(element: Element) -> Self {
+        BTree{
+            root: Node{
+            len: element.len,
+            leaf: true,
+            elements: vec![element],
+            children: vec![],
+        }}
+    }
+
+    pub fn insert(&mut self, element: Element) {
+        if self.root.is_full() {
+            let new_root = Node{len: 0, leaf: false, elements: vec![], children: vec![]};
+            let old_root = mem::replace(&mut self.root, new_root);
+            self.root.len = old_root.len;
+            self.root.children.push(old_root);
+            self.root.split_child(0);
+        }
+        self.root.insert_nonfull(element);
+    }
+
+    pub fn search(&self, index: usize) -> Result<(&Node, usize), Error> {
+        self.root.search(index)
+    }
+}
+
 
 impl Node {
     /// finds the node that contains `index`.
@@ -40,7 +72,7 @@ impl Node {
     /// Each new child has B-1 elements and B children. The original
     /// child's median element is promoted as the ith element of the
     /// parent node.
-    fn split_child(&mut self, i: usize) {
+    pub fn split_child(&mut self, i: usize) {
         let (median, new_child) = {
             let ref mut child = self.children[i];
             let mut elements = child.elements.split_off(MIN_LEN);
@@ -66,5 +98,23 @@ impl Node {
 
         self.elements.insert(i, median);
         self.children.insert(i + 1, new_child);
+    }
+
+    /// Insert a new element into a node that is not full.
+    fn insert_nonfull(&mut self, elt: Element) {
+        let mut pos = self.elements.binary_search(&elt).err().expect("Duplicate UID!");
+        if self.leaf {
+            self.elements.insert(pos, elt);
+        } else {
+            if self.children[pos].is_full() {
+                self.split_child(pos);
+                if elt > self.elements[pos] { pos += 1 }
+            }
+            self.children[pos].insert_nonfull(elt)
+        }
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.elements.len() == CAPACITY
     }
 }
