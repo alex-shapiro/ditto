e82f2fc0fe2fc08059fd74c50225bbd3096c6d03
Alex Shapiro <alexander.max.shapiro@gmail.com>
2018-01-15 21:46:30 -0800

    replace Replica with Dot

--- ditto/src/replica.rs
+++ ditto/src/replica.rs
@@ -2,6 +2,7 @@ use map_tuple_vec;
 use std::cmp::max;
 use std::collections::HashMap;
 
+pub type Dot = Replica;
 pub type SiteId = u32;
 pub type Counter = u32;
 pub type Summary = Tombstones;
@@ -16,9 +17,9 @@ pub struct Replica {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Tombstones(#[serde(with = "map_tuple_vec")] HashMap<u32,u32>);
 
-impl Replica {
-    pub fn new(site: u32, counter: u32) -> Self {
-        Replica{site: site, counter: counter}
+impl Dot {
+    pub fn new(site_id: SiteId, counter: Counter) -> Self {
+        Dot{site: site_id, counter}
     }
 }
 
@@ -31,41 +32,46 @@ impl Tombstones {
         *self.0.get(&site_id).unwrap_or(&0)
     }
 
+    pub fn get_dot(&mut self, site_id: SiteId) -> Dot {
+        let counter = self.increment(site_id);
+        Dot{site: site_id, counter}
+    }
+
     pub fn increment(&mut self, site_id: SiteId) -> Counter {
         let entry = self.0.entry(site_id).or_insert(0);
         *entry += 1;
         return *entry;
     }
 
-    pub fn contains(&self, replica: &Replica) -> bool {
-        match self.0.get(&replica.site) {
-            Some(counter) => *counter >= replica.counter,
+    pub fn contains(&self, dot: &Dot) -> bool {
+        match self.0.get(&dot.site) {
+            Some(counter) => *counter >= dot.counter,
             None => false,
         }
     }
 
-    pub fn contains_pair(&self, site: u32, counter: u32) -> bool {
-        match self.0.get(&site) {
+    pub fn contains_pair(&self, site_id: u32, counter: u32) -> bool {
+        match self.0.get(&site_id) {
             Some(site_counter) => *site_counter >= counter,
             None => false,
         }
     }
 
-    pub fn insert(&mut self, replica: &Replica) {
-        let entry = self.0.entry(replica.site).or_insert(replica.counter);
-        *entry = max(*entry, replica.counter);
+    pub fn insert(&mut self, dot: &Dot) {
+        let entry = self.0.entry(dot.site).or_insert(dot.counter);
+        *entry = max(*entry, dot.counter);
     }
 
-    pub fn insert_pair(&mut self, site: u32, counter: u32) {
-        let entry = self.0.entry(site).or_insert(counter);
+    pub fn insert_pair(&mut self, site_id: u32, counter: u32) {
+        let entry = self.0.entry(site_id).or_insert(counter);
         *entry = max(*entry, counter);
     }
 
     pub fn merge(&mut self, other: &Tombstones) {
-        for (site, counter) in &other.0 {
-            let site = *site;
+        for (site_id, counter) in &other.0 {
+            let site_id = *site_id;
             let counter = *counter;
-            let entry = self.0.entry(site).or_insert(counter);
+            let entry = self.0.entry(site_id).or_insert(counter);
             *entry = max(*entry, counter);
         }
     }
