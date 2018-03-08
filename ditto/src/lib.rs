42dc5e4d14a580666e2926836cd72f2af403dd6b
Alex Shapiro <alexander.max.shapiro@gmail.com>
2018-03-08 07:24:14 -0800

    rename order_statistic_tree -> ditto_tree

--- ditto/src/lib.rs
+++ ditto/src/lib.rs
@@ -183,7 +183,7 @@
 extern crate base64;
 #[macro_use] extern crate lazy_static;
 extern crate num;
-extern crate order_statistic_tree;
+extern crate ditto_tree;
 extern crate rand;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
